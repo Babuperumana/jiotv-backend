d6a043d4ec19f2353496e894fca8f79f8828ec70
Babuperumana <babuperumana@gmail.com>
2026-09-02 15:54:24 +0530

    Remove temporary test scripts

--- test_cache.py
+++
@@ -1,4 +0,0 @@
-import asyncio
-from main import _stream_cache
-
-print(_stream_cache)
--- test_fetch.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from main import _fetch_and_cache_stream
-
-async def test():
-    data = await _fetch_and_cache_stream("173", "http://localhost:8000")
-    print("DATA:", data)
-
-asyncio.run(test())
